e5193b7cc54c7e355e3fb2d0c32b0aba0a4feab9
邹晓杰 <zouxiaojie@zouxiaojiedeMacBook-Air.local>
2022-03-16 11:45:39 +0800

    0

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 // import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
+import App from './App.vue'
 import store from './store'
 
 const app = createApp(App)
-// app.use(ElementPlus)
-app.use(store)
 
+app.use(store)
+// app.use(ElementPlus)
 app.mount('#app')
